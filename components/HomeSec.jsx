7a6913976ed159aab22bab287f63ac8ca029493b
shing1Sks <shreyashks02@gmail.com>
2024-08-24 22:56:00 +0530

    ui fixes

--- components/HomeSec.jsx
+++ components/HomeSec.jsx
@@ -5,14 +5,14 @@ function HomeSec() {
     <div className="text-black w-full items-center flex flex-row py-[5%] justify-between">
       <img
         src="./assetleft.webp"
-        className="h-[500px] rounded-xl pt-2"
+        className="h-[550px] rounded-xl pt-2"
         alt=""
       />
       <div className="flex flex-col items-center justify-center">
-        <p className="text-7xl text-center font-[400] pb-2">
+        <p className="text-7xl text-center font-[400] pb-7">
           Discover the Power of Innovation: Connect
         </p>
-        <p className="text-xl pb-2">
+        <p className="text-2xl text-center px-[10%] pb-5 text-gray-600">
           Unlock a New World of Possibilities: Explore Our Platform and Unleash
           Your Entrepreneurial Spirit
         </p>
@@ -22,7 +22,7 @@ function HomeSec() {
       </div>
       <img
         src="./assetright.webp"
-        className="h-[500px] rounded-xl pt-2"
+        className="h-[550px] rounded-xl pt-2"
         alt=""
       />
     </div>
