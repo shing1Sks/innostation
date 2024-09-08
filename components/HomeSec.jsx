9fd13c15bade143b6f67823ef1a2928de11705c7
shing1Sks <shreyashks02@gmail.com>
2024-09-08 22:31:17 +0530

    updates further

--- components/HomeSec.jsx
+++ components/HomeSec.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Page1 from "./Home/Page1";
 import Page2 from "./Home/Page2";
+import { Page3 } from "./Home/Page3";
 
 function HomeSec() {
   return (
@@ -9,6 +10,7 @@ function HomeSec() {
       <Header />
       <Page1 />
       <Page2 />
+      <Page3 />
     </div>
   );
 }
